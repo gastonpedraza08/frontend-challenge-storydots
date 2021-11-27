3fe6201694b5cb56ad984c4a5d4688d213687d36
Gaston Pedraza <sleepwalkgaston08@gmail.com>
2021-11-27 16:46:07 -0300

    add normalize

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+
 import AppRoutes from './Routes';
 
 export default function App() {
   return (
     <>
+    	<CssBaseline />
       <AppRoutes />
     </>
   );
