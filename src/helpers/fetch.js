633aac7ba70e07fd40cbe3015efe500f077ef4fe
Gaston Pedraza <sleepwalkgaston08@gmail.com>
2021-11-27 17:49:54 -0300

    fix

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,15 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 let baseUrl;
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
 	baseUrl = process.env.REACT_APP_API_URL_DEV;
 } else {
 	baseUrl = process.env.REACT_APP_API_URL;
 }
 
-export const fetchWithoutToken = async (endpoint, data, method = "GET") => {
+export const fetchWithoutToken = async (endpoint, data, method = 'GET') => {
   try {
     const url = `${baseUrl}/${endpoint}`;
-    if (method === "GET") {
+    if (method === 'GET') {
       const result = await axios({
         method,
         url,
@@ -18,7 +18,7 @@ export const fetchWithoutToken = async (endpoint, data, method = "GET") => {
     } else {
       const result = await axios({
         headers: {
-          "content-type": "application/json",
+          'content-type': 'application/json',
         },
         method,
         url,
@@ -31,9 +31,9 @@ export const fetchWithoutToken = async (endpoint, data, method = "GET") => {
       return {
         error: error.response.data.error
           ? error.response.data.error
-          : "Error en el servidor",
+          : 'Error en el servidor',
       };
     }
-    return { error: "Error en el servidor " };
+    return { error: 'Error en el servidor ' };
   }
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,19 @@
 import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
-import { fetchWithoutToken } from '../helpers/fetch';
+import { fetchWithoutToken } from 'helpers/fetch';
+import { productsLoadAllproducts } from 'actions/ui';
 
 export default function Home() {
 
+	const dispatch = useDispatch();
+	const { products } = useSelector(state => state.products);
+
 	useEffect(async () => {
-		const result = await fetchWithoutToken('products');
-		console.log(result)
-	}, []);
+		dispatch(productsLoadAllproducts());
+	}, [dispatch]);
+
+	console.log(products)
 
 	return (
 		<div>
